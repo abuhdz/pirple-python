3f911f1c9133753844914455e39b42094e8c912b
secura <secura@MacBook-Pro-Abu.local>
2019-04-06 18:36:16 +0700

    First Home work - Variable

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+Homework #1
+Script by : Abu Hdz - 20190406
+
+Song Attribute
+Single by OneRepublic
+Album Dreaming Out Loud
+"""
+
+# Assign variable
+album = "Dreaming Out Loud"
+release = 2006
+genre = "Pop roc - R&B"
+length = "3:28"
+label = "Mosley - Interscope"
+songWriter = "Ryan Tedder"
+producer = "Greg Wells"
+
+# Print to screen
+print ("This is Wiki about song 'Appologize' by OneRepublic")
+print ("-----------------------------------------------------")
+print ("Album        : " + album)
+print ("Release      : " + str(release))
+print ("Genre        : " + genre) 
+print ("Song length  : " + length)
+print ("Label        : " + label)
+print ("Song Writer  : " + songWriter)
+print ("Producer     : " + producer)
